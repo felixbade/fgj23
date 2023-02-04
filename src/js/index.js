a41c92ebb2dfe232286213c9d9e6d387833fa0bc
Felix Bade <flix.bade@gmail.com>
2023-02-04 21:15:38 +0200

    Add a growable root

--- src/js/index.js
+++ src/js/index.js
@@ -8,7 +8,7 @@ window.addEventListener('load', () => {
 
     const rootSprites = []
     const rootContainer = new PIXI.Container()
-    rootContainer.scale.set(0.3)
+    rootContainer.scale.set(0.2)
     rootContainer.y = -400
     container.addChild(rootContainer)
 
@@ -24,13 +24,31 @@ window.addEventListener('load', () => {
     }
 
     app.ticker.add((delta) => {
-        x += controller.move.x * delta * 0.3
+        x += controller.move.x * delta * 0.4
         // y += controller.move.y * delta * 10
-        // console.log(controller.trigger)
+
+        if (controller.trigger) {
+            y += delta * 0.3
+        }
+
+        const scaling = Math.min(1, 0.95 + 0.05 * y)
+        rootSprites[rootSprites.length - 20].scale.set(scaling)
+
+        if (y >= 1) {
+            y -= 1
+            let sprite = PIXI.Sprite.from('assets/images/white square.png')
+            sprite.anchor.set(0.5)
+            sprite.scale.set(0.95)
+            sprite.y = 64
+            rootSprites[rootSprites.length - 1].addChild(sprite)
+            rootSprites.push(sprite)
+        }
 
         for (let i = 0; i < rootSprites.length; i++) {
             const sprite = rootSprites[i]
-            sprite.angle = -x
+            if (rootSprites.length - 1 - i < 20) {
+                sprite.angle = -x
+            }
         }
     })
 })
