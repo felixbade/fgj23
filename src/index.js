b48a5211dee85fc248a7e0f6e58d2c847d90c204
Felix Bade <flix.bade@gmail.com>
2023-02-04 15:13:12 +0200

    No ; at the end of the line

--- src/index.js
+++ src/index.js
@@ -9,43 +9,43 @@ window.addEventListener('load', () => {
         fsButton.style.display = 'none'
     }
 
-// Create the application helper and add its render target to the page
-const app = new PIXI.Application({ width: 640, height: 360 });
-document.body.appendChild(app.view);
+    // Create the application helper and add its render target to the page
+    const app = new PIXI.Application({ width: 640, height: 360 })
+    document.body.appendChild(app.view)
 
-// Add a container to center our sprite stack on the page
-const container = new PIXI.Container();
-container.x = app.screen.width / 2;
-container.y = app.screen.height / 2;
-app.stage.addChild(container);
+    // Add a container to center our sprite stack on the page
+    const container = new PIXI.Container()
+    container.x = app.screen.width / 2
+    container.y = app.screen.height / 2
+    app.stage.addChild(container)
 
-// Create the 3 sprites, each a child of the last
-const sprites = [];
-let parent = container;
-for (let i = 0; i < 3; i++) {
-  let sprite = PIXI.Sprite.from('assets/images/sample.png');
-  sprite.anchor.set(0.5);
-  parent.addChild(sprite);
-  sprites.push(sprite);
-  parent = sprite;
-}
+    // Create the 3 sprites, each a child of the last
+    const sprites = []
+    let parent = container
+    for (let i = 0; i < 3; i++) {
+        let sprite = PIXI.Sprite.from('assets/images/sample.png')
+        sprite.anchor.set(0.5)
+        parent.addChild(sprite)
+        sprites.push(sprite)
+        parent = sprite
+    }
 
-// Set all sprite's properties to the same value, animated over time
-let elapsed = 0.0;
-app.ticker.add((delta) => {
-  elapsed += delta / 60;
-  const amount = Math.sin(elapsed);
-  const scale = 1.0 + 0.25 * amount;
-  const alpha = 0.75 + 0.25 * amount;
-  const angle = 40 * amount;
-  const x = 75 * amount;
-  for (let i = 0; i < sprites.length; i++) {
-    const sprite = sprites[i];
-    sprite.scale.set(scale);
-    sprite.alpha = alpha;
-    sprite.angle = angle;
-    sprite.x = x;
-  }
-});
+    // Set all sprite's properties to the same value, animated over time
+    let elapsed = 0.0
+    app.ticker.add((delta) => {
+        elapsed += delta / 60
+        const amount = Math.sin(elapsed)
+        const scale = 1.0 + 0.25 * amount
+        const alpha = 0.75 + 0.25 * amount
+        const angle = 40 * amount
+        const x = 75 * amount
+        for (let i = 0; i < sprites.length; i++) {
+            const sprite = sprites[i]
+            sprite.scale.set(scale)
+            sprite.alpha = alpha
+            sprite.angle = angle
+            sprite.x = x
+        }
+    })
 
 })
