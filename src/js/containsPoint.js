941f8805bf5707b62aaf7ef45090ed2b587f9175
Felix Bade <flix.bade@gmail.com>
2023-02-05 13:10:10 +0200

    Remove ; from EOL

--- src/js/containsPoint.js
+++ src/js/containsPoint.js
@@ -1,20 +1,20 @@
 export const collider = function (sprite, point) {
     const tempPoint = {x: 0, y: 0 }
     //get mouse poisition relative to the bunny anchor point
-    sprite.worldTransform.applyInverse(point, tempPoint);
+    sprite.worldTransform.applyInverse(point, tempPoint)
 
-    const width = sprite._texture.orig.width;
-    const height = sprite._texture.orig.height;
-    const x1 = -width * sprite.anchor.x;
-    let y1 = 0;
+    const width = sprite._texture.orig.width
+    const height = sprite._texture.orig.height
+    const x1 = -width * sprite.anchor.x
+    let y1 = 0
 
-    let flag = false;
+    let flag = false
     //collision detection for sprite (as a square, not pixel perfect)
     if (tempPoint.x >= x1 && tempPoint.x < x1 + width) {
-        y1 = -height * sprite.anchor.y;
+        y1 = -height * sprite.anchor.y
 
         if (tempPoint.y >= y1 && tempPoint.y < y1 + height) {
-            flag = true;
+            flag = true
         }
     }
     //if collision not detected return false
@@ -25,71 +25,71 @@ export const collider = function (sprite, point) {
     //if not continues from here
 
     // bitmap check
-    const tex = sprite.texture;
-    const baseTex = sprite.texture.baseTexture;
-    const res = baseTex.resolution;
+    const tex = sprite.texture
+    const baseTex = sprite.texture.baseTexture
+    const res = baseTex.resolution
 
     if (!baseTex.hitmap) {
         //generate hitmap
         if (!genHitmap(baseTex, 255)) {
-            return true;
+            return true
         }
 
     }
 
-    const hitmap = baseTex.hitmap;
+    const hitmap = baseTex.hitmap
 
     // this does not account for rotation yet!!!
 
     //check mouse position if its over the sprite and visible
-    let dx = Math.round((tempPoint.x - x1 + tex.frame.x) * res);
-    let dy = Math.round((tempPoint.y - y1 + tex.frame.y) * res);
-    let ind = (dx + dy * baseTex.realWidth);
-    let ind1 = ind % 32;
-    let ind2 = ind / 32 | 0;
-    return (hitmap[ind2] & (1 << ind1)) !== 0;
+    let dx = Math.round((tempPoint.x - x1 + tex.frame.x) * res)
+    let dy = Math.round((tempPoint.y - y1 + tex.frame.y) * res)
+    let ind = (dx + dy * baseTex.realWidth)
+    let ind1 = ind % 32
+    let ind2 = ind / 32 | 0
+    return (hitmap[ind2] & (1 << ind1)) !== 0
 }
 
 function genHitmap(baseTex, threshold) {
     //check sprite props
     if (!baseTex.resource) {
         //renderTexture
-        return false;
+        return false
     }
-    const imgSource = baseTex.resource.source;
-    let canvas = null;
+    const imgSource = baseTex.resource.source
+    let canvas = null
     if (!imgSource) {
-        return false;
+        return false
     }
-    let context = null;
+    let context = null
     if (imgSource.getContext) {
-        canvas = imgSource;
-        context = canvas.getContext('2d');
+        canvas = imgSource
+        context = canvas.getContext('2d')
     } else if (imgSource instanceof Image) {
-        canvas = document.createElement('canvas');
-        canvas.width = imgSource.width;
-        canvas.height = imgSource.height;
-        context = canvas.getContext('2d');
-        context.drawImage(imgSource, 0, 0);
+        canvas = document.createElement('canvas')
+        canvas.width = imgSource.width
+        canvas.height = imgSource.height
+        context = canvas.getContext('2d')
+        context.drawImage(imgSource, 0, 0)
     } else {
         //unknown source;
-        return false;
+        return false
     }
 
-    const w = canvas.width, h = canvas.height;
-    let imageData = context.getImageData(0, 0, w, h);
+    const w = canvas.width, h = canvas.height
+    let imageData = context.getImageData(0, 0, w, h)
     //create array
-    let hitmap = baseTex.hitmap = new Uint32Array(Math.ceil(w * h / 32));
+    let hitmap = baseTex.hitmap = new Uint32Array(Math.ceil(w * h / 32))
     //fill array
     for (let i = 0; i < w * h; i++) {
         //lower resolution to make it faster
-        let ind1 = i % 32;
-        let ind2 = i / 32 | 0;
+        let ind1 = i % 32
+        let ind2 = i / 32 | 0
         //check every 4th value of image data (alpha number; opacity of the pixel)
         //if it's visible add to the array
         if (imageData.data[i * 4 + 3] >= threshold) {
-            hitmap[ind2] = hitmap[ind2] | (1 << ind1);
+            hitmap[ind2] = hitmap[ind2] | (1 << ind1)
         }
     }
-    return true;
+    return true
 }
